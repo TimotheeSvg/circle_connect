58a919f1837da5368b8d7718dc1e9a06e540acdd
TimotheeSvg <ssauvage.timothee@gmail.com>
2024-03-07 12:33:21 +0100

    init

---
+++ src/main/java/fr/esgi/circle_connect/CircleConnectApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.circle_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/circle_connect/CircleConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.circle_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
